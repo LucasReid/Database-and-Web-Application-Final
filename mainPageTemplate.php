1014d6a7a1c8f38a68a85444302a266435656810
lvandament <lvandament@users.noreply.github.com>
2017-03-06 11:55:20 -0500

    UpdateMainPage.php
    PSST

--- mainPageTemplate.php
+++ mainPageTemplate.php
@@ -8,6 +8,7 @@ or die ('Error connecting to MySqL Server.');
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
 <link rel="stylesheet" type="text/css" href="mainCSS.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="webJS.js" defer></script>
 </head>
 
