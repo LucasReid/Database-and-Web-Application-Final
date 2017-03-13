2231fa1d2876e51508f52ab96cbd415ecce56b62
LucasReid <lukereid7466@yahoo.com>
2017-03-12 20:53:52 -0400

    Add files via upload

--- memberPage_comp305.php
+++ memberPage_comp305.php
@@ -5,26 +5,27 @@
   $Title="Member Information";
   $Subtitle="";
 
-print_r($_SESSION);
 //only good if register first
 if($_SESSION['Member_id']!==null)
 {
-echo " <p>\n";
-echo "  Current Member ID: ".$_SESSION["Member_id"]."\n";
-echo " <br />";
-echo "  Current Fraternity / Sorority ID: ".$_SESSION["FoS_id"]."\n";
-echo " <br />";
-echo "  Current Member Name: ".$_SESSION["Member_name"]."\n";
-echo " <br />";
-echo "  Current Grad Year: ".$_SESSION["Grad_Year"]."\n";
-echo " <br />";
-echo "  Current Email: ".$_SESSION["Email"]."\n";
-echo " <br />";
-echo "  Current Admin Status: ".$_SESSION["Admin_status"]."\n";
-echo " <br />";
-echo "  Current Dues: ".$_SESSION["Dues_owed"]."\n";
-echo " <br />";
-echo " </p>";
+echo " <a href='logIn.php'>Back</a>";    
+echo " <h2 class='w3-center w3-padding-32 w3-text-white w3-xlarge' style='background-color:#821717'> Welcome ".$_SESSION["Member_name"]." </h2>";
+echo " <table class='w3-table-all w3-centered' style='width:75%; margin-left:auto;margin-right:auto'>";
+echo " <th> Current Member ID";
+echo " <th> Current Fraternity / Sorority ID ";
+echo " <th> Member Name "; 
+echo " <th> Grad Year ";
+echo " <th> Email "; 
+echo " <th> Admin Status "; 
+echo " <th> Dues </th>"; 
+echo " <tr><td>".$_SESSION["Member_id"]."</td>";
+echo " <td>".$_SESSION["FoS_id"]."</td>";
+echo " <td>".$_SESSION["Member_name"]."</td>"; 
+echo " <td>".$_SESSION["Grad_Year"]."</td>";     
+echo " <td>".$_SESSION["Email"]."</td>"; 
+echo " <td>".$_SESSION["Admin_status"]."</td>"; 
+echo " <td>".$_SESSION["Dues_owed"]."</td></tr>";     
+echo " </table>";    
 }
 else
 {
@@ -32,6 +33,7 @@ else
   echo "<br />";
   echo '<a href="logIn.php"> Log in</a>';
 }
+echo " <h4 class='w3-center w3-padding-16'> Member Info Table </h4>";
  WriteRegistrationINFO();
 ?>
 <!DOCTYPE html>
