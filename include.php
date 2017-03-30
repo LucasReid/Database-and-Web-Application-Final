d4f6948e7406ea77219e4f3a8d4cc0c095146efc
apeaseyo <aap5264@lhup.edu>
2017-03-30 19:29:10 -0400

    Correct session code

--- include.php
+++ include.php
@@ -1,19 +1,17 @@
 <?php
 
-function Footer()
-{
 // check for a user
-if ( isset( $_SESSION["UserName"] ))
+if ( isset( $_SESSION["Member_id"] ))
 {
     // echo the current user
-    echo "   <p>Current User: ".$_SESSION["UserName"]."</p>\n";
-    echo "   <p><a href=\"LogOut.php\">Log Out</a> when finished</p>\n";
+    echo "   <p>Current User: ".$_SESSION["Member_name"]."</p>\n";
+    echo "   <p><a href=\"logOut.php\">Log Out</a> when finished</p>\n";
 }
 else
 {
     // echo no login yet
     echo "   <p>No user logged in</p>\n";
-    echo "   <p>Please <a href=\"LogOn.php\">Log in</a></p>\n";
+    echo "   <p>Please <a href=\"logOn.php\">Log in</a></p>\n";
 }
 	echo "</div>\n";
 }
