ff0ae8b3ac26129bda9838152b06ed989063f804
lvandament <lvandament@users.noreply.github.com>
2017-04-26 11:29:42 -0400

    Update UserPage.php

--- UserPage.php
+++ UserPage.php
@@ -65,37 +65,7 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Open Sans", sans-serif}
                         $fos = $_SESSION['FoS_id'];
                         $pass = $_SESSION['Password'];
                     ?>
-                    <script>
-                        var fos = "<?php echo $fos ?>";
-                        
-                        var imgArray = new Array();
-
-                        imgArray[1] = new Image();
-                        imgArray[1].src ='pictures/ACR.jpg'; 
-                        
-                        imgArray[2] = new Image();
-                        imgArray[2].src ='pictures/ASP.jpg'; 
-                        
-                        imgArray[3] = new Image();
-                        imgArray[3].src ='pictures/KDR.jpg'; 
-                        
-                        imgArray[4] = new Image();
-                        imgArray[4].src ='pictures/PMD.jpg'; 
-                        
-                        imgArray[5] = new Image();
-                        imgArray[5].src ='pictures/AST.jpg'; 
-                        
-                        imgArray[6] = new Image();
-                        imgArray[6].src ='pictures/SK.jpg'; 
-                        
-                        imgArray[7] = new Image();
-                        imgArray[7].src ='pictures/SSS.jpg'; 
-                        
-                        imgArray[8] = new Image();
-                        imgArray[8].src ='pictures/ZTA.jpg';
-                        
-                        document.write("<img src='"+imgArray[fos].src+"'/>");
-                    </script>
+                    
                 </div>
                 <div class="w3-card-4" style="padding:15px;">
                     <h5 class="w3-opacity" >Lock Haven University of Pennsylvania Sorority and Faterinty</h5>
@@ -152,10 +122,41 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Open Sans", sans-serif}
             <?php
                 echo"<p>Welcome: ".$_SESSION['Member_name']."</p>";
             ?>
+            <script>
+                        var fos = "<?php echo $fos ?>";
+                        
+                        var imgArray = new Array();
+
+                        imgArray[1] = new Image();
+                        imgArray[1].src ='pictures/ACR.jpg'; 
+                        
+                        imgArray[2] = new Image();
+                        imgArray[2].src ='pictures/ASP.jpg'; 
+                        
+                        imgArray[3] = new Image();
+                        imgArray[3].src ='pictures/KDR.jpg'; 
+                        
+                        imgArray[4] = new Image();
+                        imgArray[4].src ='pictures/PMD.jpg'; 
+                        
+                        imgArray[5] = new Image();
+                        imgArray[5].src ='pictures/AST.jpg'; 
+                        
+                        imgArray[6] = new Image();
+                        imgArray[6].src ='pictures/SK.jpg'; 
+                        
+                        imgArray[7] = new Image();
+                        imgArray[7].src ='pictures/SSS.jpg'; 
+                        
+                        imgArray[8] = new Image();
+                        imgArray[8].src ='pictures/ZTA.jpg';
+                        
+                        document.write("<img src='"+imgArray[fos].src+"'/>");
+                    </script>
           <hr>
           <p><button class="w3-button w3-block w3-theme-l4">Learn More</button></p>
           <!-- Profile -->
-            <p> This is where the outline of the fraternity or sorority would pop up and be nice to look at. with link to fb and twitter.</p>
+           // <p> This is where the outline of the fraternity or sorority would pop up and be nice to look at. with link to fb and twitter.</p>
               <hr>
             	  <a href="logOut.php"><button type="button" class="w3-button w3-theme" ><i class="fa fa-pencil"></i>  Log Out</button></a>
             </div>
